004f9fcb5002f7106653f59e66487d2c54864c87
mukesh swami <mukesh.swami.501598@gmail.com>
2021-09-09 08:54:06 +0530

    Constants

--- src/com/learn/kotlin/constants/Constants.kt
+++ src/com/learn/kotlin/constants/Constants.kt
@@ -4,7 +4,7 @@ const val MIN_PER_VALUE = 0
 val MAX_PER_VALUE = Constants().getMaxPer()
 
 class Constants {
-    
+
     fun printMinAndMaxPercentage() {
         println("Min :- ${MIN_PER_VALUE} , Max :- ${MAX_PER_VALUE}")
     }
