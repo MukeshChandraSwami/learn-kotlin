ae3a0b490e45f1afd03b3128c5c81c8536233ac5
mukesh swami <mukesh.swami.501598@gmail.com>
2021-09-20 09:33:05 +0530

    Singletons in kotlin

---
+++ src/com/learn/kotlin/singletons/Singletons.kt
@@ -0,0 +1,19 @@
+package com.learn.kotlin.singletons
+
+fun main() {
+
+    val s1 = Singletons
+    val s2 = Singletons
+
+    println(s1)
+    println(s2)
+
+    println(s1 === s2)
+}
+
+object Singletons {
+
+    override fun toString(): String {
+        return "I am singleton type."
+    }
+}
